47ea4e2240466c8e22f29d0a264e0f1f09eb6540
Seok-jun.Kang <kangsj0327@gmail.com>
2025-06-22 22:37:25 +0900

    add comment to minHeap and add buildHeap method

--- src/sort/minHeap.ts
+++ src/sort/minHeap.ts
@@ -1,5 +1,25 @@
+// MinHeap은 부모 노드가 자식 노드보다 값이 작다는 조건을 가진 이진 트리이다.
+// 힙의 루트 노드에는 항상 가장 작은 값이 저장된다.
+// 힙은 Priority Queue(dequeue 시 가장 높은 우선순위를 반환 = 힙의 루트를 반환)를 효율적으로 구현할 수 있는 대표적인 방법이다.
+// 힙 생성: O(N) 삽입: O(logN) 삭제: O(logN) 조회: O(1)
+// 힙으로 정렬(루트 노드를 빼서 순서대로 두면 된다) 시 시간 복잡도는 O(NlogN) (삭제 연산을 노드 수만큼 실행)
+
 class MinHeap {
   private heap: number[] = [];
+
+  constructor(array: number[] = []) {
+    if (array.length > 0) {
+      this.buildHeap(array);
+    }
+  }
+
+  private buildHeap(array: number[]): void {
+    this.heap = array.slice();
+    const start = Math.floor((this.heap.length - 2) / 2);
+    for (let i = start; i >= 0; i--) {
+      this.heapifyDown(i);
+    }
+  }
 
   private getParentIndex(i: number): number {
     return Math.floor((i - 1) / 2);
